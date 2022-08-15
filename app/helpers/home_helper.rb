d82beec5ce6e4f6b0f7aba2e04260b9ddc1315b6
Nonexistent <nx@example.org>
2022-08-16 02:03:16 +0800

    Optimize HomeHelper#account_link_to

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -8,30 +8,44 @@ module HomeHelper
   end
 
   def account_link_to(account, button = '', path: nil)
+    def missing_account_section
+      content_tag(:div,
+                  content_tag(:div,
+                              image_tag(full_asset_url('avatars/original/missing.png', skip_pipeline: true),
+                                        class: 'account__avatar'),
+                              class: 'account__avatar-wrapper') <<
+                  content_tag(:span,
+                              content_tag(:strong,
+                                          t('about.contact_missing')) <<
+                              content_tag(:span,
+                                          t('about.contact_unavailable'),
+                                          class: 'display-name__account'),
+                              class: 'display-name'),
+                  class: 'account__display-name')
+    end
+
+    def account_section(account)
+      content_tag(:div,
+                  image_tag(full_asset_url(current_account&.user&.setting_auto_play_gif ? account.avatar_original_url : account.avatar_static_url),
+                            class: 'account__avatar'),
+                  class: 'account__avatar-wrapper') <<
+      content_tag(:span,
+                  content_tag(:bdi,
+                              content_tag(:strong,
+                                          display_name(account, custom_emojify: true),
+                                          class: 'display-name__html emojify')) <<
+                  content_tag(:span,
+                              "@#{account.acct}",
+                              class: 'display-name__account'),
+                  class: 'display-name')
+    end
+
     content_tag(:div, class: 'account') do
       content_tag(:div, class: 'account__wrapper') do
         section = if account.nil?
-                    content_tag(:div, class: 'account__display-name') do
-                      content_tag(:div, class: 'account__avatar-wrapper') do
-                        image_tag(full_asset_url('avatars/original/missing.png', skip_pipeline: true), class: 'account__avatar')
-                      end +
-                        content_tag(:span, class: 'display-name') do
-                          content_tag(:strong, t('about.contact_missing')) +
-                            content_tag(:span, t('about.contact_unavailable'), class: 'display-name__account')
-                        end
-                    end
+                    missing_account_section
                   else
-                    link_to(path || ActivityPub::TagManager.instance.url_for(account), class: 'account__display-name') do
-                      content_tag(:div, class: 'account__avatar-wrapper') do
-                        image_tag(full_asset_url(current_account&.user&.setting_auto_play_gif ? account.avatar_original_url : account.avatar_static_url), class: 'account__avatar')
-                      end +
-                        content_tag(:span, class: 'display-name') do
-                          content_tag(:bdi) do
-                            content_tag(:strong, display_name(account, custom_emojify: true), class: 'display-name__html emojify')
-                          end +
-                            content_tag(:span, "@#{account.acct}", class: 'display-name__account')
-                        end
-                    end
+                    link_to(account_section(account), path || ActivityPub::TagManager.instance.url_for(account), class: 'account__display-name')
                   end
 
         section + button
