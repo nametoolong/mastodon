da088ececb6da47ebbb055c06d5d65025b0526c0
Nonexistent <nx@example.org>
2022-08-29 17:37:42 +0800

    Remove redundant includes

--- app/models/status.rb
+++ app/models/status.rb
@@ -356,7 +356,7 @@ class Status < ApplicationRecord
 
       return if account_ids.empty?
 
-      accounts = Account.where(id: account_ids.to_a).includes(:account_stat, :user).eager_load(:account_stat, :user).index_by(&:id)
+      accounts = Account.where(id: account_ids.to_a).eager_load(:account_stat, :user).index_by(&:id)
 
       cached_items.each do |item|
         item.account = accounts[item.account_id]
