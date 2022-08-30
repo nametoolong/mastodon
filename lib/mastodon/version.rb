d43f6a1f2bbdeffb6d6c677d979b6560c8e0d007
Nonexistent <nx@example.org>
2022-08-31 00:30:24 +0800

    Rebranding

--- lib/mastodon/version.rb
+++ lib/mastodon/version.rb
@@ -21,7 +21,7 @@ module Mastodon
     end
 
     def suffix
-      ''
+      '/mastoduck'
     end
 
     def to_a
@@ -33,7 +33,7 @@ module Mastodon
     end
 
     def repository
-      ENV.fetch('GITHUB_REPOSITORY', 'mastodon/mastodon')
+      ENV.fetch('GITHUB_REPOSITORY', 'nametoolong/mastodon')
     end
 
     def source_base_url
