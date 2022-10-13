72aac3b07bfc18517c5d8cd64ff955844d6dbaae
Eugen Rochko <eugen@zeonfederated.com>
2022-10-13 11:29:19 +0200

    Add image processing and generate blurhash for server thumbnail (#19348)
    Remove separate server hero setting

--- app/presenters/instance_presenter.rb
+++ app/presenters/instance_presenter.rb
@@ -78,10 +78,6 @@ class InstancePresenter < ActiveModelSerializers::Model
     @thumbnail ||= Rails.cache.fetch('site_uploads/thumbnail') { SiteUpload.find_by(var: 'thumbnail') }
   end
 
-  def hero
-    @hero ||= Rails.cache.fetch('site_uploads/hero') { SiteUpload.find_by(var: 'hero') }
-  end
-
   def mascot
     @mascot ||= Rails.cache.fetch('site_uploads/mascot') { SiteUpload.find_by(var: 'mascot') }
   end
