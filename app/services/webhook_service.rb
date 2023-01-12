dba8ed0dad84ad7e877076c6d19045f439fc9277
Nonexistent <nx@example.org>
2023-01-12 13:44:37 +0800

    Avoid serialization when no webhook exists

--- app/services/webhook_service.rb
+++ app/services/webhook_service.rb
@@ -1,20 +1,16 @@
 # frozen_string_literal: true
 
 class WebhookService < BaseService
-  def call(event, object)
+  def call(event, object, webhooks)
     @event  = Webhooks::EventPresenter.new(event, object)
     @body   = serialize_event
 
-    webhooks_for_event.each do |webhook_id|
+    webhooks.each do |webhook_id|
       Webhooks::DeliveryWorker.perform_async(webhook_id, @body)
     end
   end
 
   private
-
-  def webhooks_for_event
-    Webhook.enabled.where('? = ANY(events)', @event.type).pluck(:id)
-  end
 
   def serialize_event
     Oj.dump(ActiveModelSerializers::SerializableResource.new(@event, serializer: REST::Admin::WebhookEventSerializer, scope: nil, scope_name: :current_user).as_json)
--- app/workers/trigger_webhook_worker.rb
+++ app/workers/trigger_webhook_worker.rb
@@ -4,8 +4,12 @@ class TriggerWebhookWorker
   include Sidekiq::Worker
 
   def perform(event, class_name, id)
+    webhooks = Webhook.enabled.where('? = ANY(events)', event).pluck(:id)
+
+    return if webhooks.empty?
+
     object = class_name.constantize.find(id)
-    WebhookService.new.call(event, object)
+    WebhookService.new.call(event, object, webhooks)
   rescue ActiveRecord::RecordNotFound
     true
   end
