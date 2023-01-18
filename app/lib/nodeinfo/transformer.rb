caa0e7568433163f0ad941a8f3eb72c3e3b7d77d
Nonexistent <nx@example.org>
2023-01-18 12:45:14 +0800

    Optimize nodeinfo key transformer

--- app/lib/nodeinfo/transformer.rb
+++ app/lib/nodeinfo/transformer.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 class NodeInfo::Transformer < Blueprinter::Transformer
+  @@node_info_key_cache = {}
+
   def transform(hash, _object, _options)
-    CaseTransform.camel_lower(hash)
+    hash.deep_transform_keys! do |key|
+      @@node_info_key_cache[key] ||= key.to_s.camelize(:lower)
+    end
   end
 end
