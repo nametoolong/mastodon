afe3b50daee3ce47e68ec94e8025f3d8828369c7
Nonexistent <nx@example.org>
2023-01-18 20:20:32 +0800

    Refactor blueprint helper

--- app/helpers/blueprint_helper.rb
+++ app/helpers/blueprint_helper.rb
@@ -2,22 +2,22 @@
 
 module BlueprintHelper
   def render_blueprint_with_account(klass, target, **kwargs)
-    if current_account.nil?
-      kwargs.merge!(view: :guest)
-    else
-      kwargs.merge!(view: :logged_in, current_account: current_account)
-    end
-
+    kwargs.merge!(blueprint_options_by_account)
     klass.render(target, **kwargs)
   end
 
   def render_as_json_with_account(klass, target, **kwargs)
-    if current_user.nil?
-      kwargs.merge!(view: :guest)
-    else
-      kwargs.merge!(view: :logged_in, current_account: current_user.account)
-    end
-
+    kwargs.merge!(blueprint_options_by_account)
     klass.render_as_json(target, **kwargs)
+  end
+
+  private
+
+  def blueprint_options_by_account
+    if current_account.nil?
+      {view: :guest}
+    else
+      {view: :logged_in, current_account: current_account}
+    end
   end
 end
